d1ad4beb72734c02f25f62116954661a9d4d68f9
Florencia Comuzzi <florenciabsalinas@gmail.com>
2025-04-28 14:21:04 -0400

    Refactor HTML parsing and S3 download functionality

--- integration-exercise.py
+++ integration-exercise.py
@@ -1,85 +1,24 @@
 import csv
-import requests
-from bs4 import BeautifulSoup
 
-# SOURCE_URL = 'https://bitbucket.org/cityhive/jobs/src/master/integration-eng/integration-entryfile.html'
-SOURCE_URL = 'https://github.com/florenciacomuzzi/integration-eng-exercise-python/blob/main/integration-entryfile.html'
+from integration_eng_exercise_python.inventory_utils import extract_inventory_object_details, \
+    process_line
+from integration_eng_exercise_python.s3_helper import S3Helper
+
 
 # 1. Using requests or any other HTTP library, grab the file HTML from: https://bitbucket.org/cityhive/jobs/src/master/integration-eng/integration-entryfile.html
 # 2. Then, parse the URL for the csv file located in the S3 bucket (as part of the script, not by hand)
 # 3. Make a GET request to Amazon's S3 with the details from #2 and save the to `local_file_path`
-initial_html_file="url-to-html-file"
-local_file_path = "path-to-exported-file"
-
-
-def process_line(line):
-  if len(line) > 10:
-    return {
-      'upc': line[0],
-      'price': line[4],
-      'quantity': line[5]
-    }
-
-
-def fix_nested_elements(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Find all elements with class 'path'
-    path_elements = soup.find_all(class_='path')
-
-    # Track elements that need to be fixed
-    elements_to_fix = []
-
-    for element in path_elements:
-        # Check if this element has a parent with the same class
-        parent = element.parent
-        if parent and 'class' in parent.attrs and 'path' in parent.attrs['class']:
-            elements_to_fix.append((parent, element))
-
-    # Fix the nested elements
-    for parent, child in elements_to_fix:
-        # Move the child's content to the parent
-        parent.string = child.string
-        # Remove the child element
-        child.decompose()
-
-    # return str(soup)
-    return soup
-
-def get_html(url):
-    response = requests.get(url)
-    response.raise_for_status()  # Raise an exception for bad status codes
-    
-    soup = fix_nested_elements(response.text)
-    
-    # Extract S3 bucket information
-    bucket_div = soup.find(id='bucket-value')
-    region_div = soup.find(id='region-value')
-    object_div = soup.find(id='object-value')
-    
-    if not all([bucket_div, region_div, object_div]):
-        raise ValueError("Could not find all required S3 information in the HTML")
-    
-    bucket = bucket_div.text.strip()
-    region_code = region_div.get('data-region', '')  # Get the actual AWS region code
-    object_path = object_div.text.strip()
-
-    # Clean up the object path by removing the path separators and extra whitespace
-    object_path = ' '.join(object_path.split())
-    
-    return {
-        'bucket': bucket,
-        'region_code': region_code,
-        'object_path': object_path
-    }
+initial_html_file='https://bitbucket.org/cityhive/jobs/src/master/integration-eng/integration-entryfile.html'
+local_file_path = 'inventory_export.csv'
 
 
 if __name__ == '__main__':
-  s3_details = get_html(SOURCE_URL)
-  # with open(local_file_path, 'r') as in_file:
-  #   reader = csv.reader(in_file, delimiter='|')
-  #   for line in reader:
-  #     l = process_line(line)
-  #     if l: print(l)
+  s3_details = extract_inventory_object_details(initial_html_file)
+  bucket = S3Helper(s3_details['bucket'], s3_details['region_code'])
+  bucket.download_key_with_presigned_url(s3_details['object_path'], local_file_path)
 
-  print(s3_details)
+  with open(local_file_path, 'r') as in_file:
+    reader = csv.reader(in_file, delimiter='|')
+    for line in reader:
+      l = process_line(line)
+      if l: print(l)
--- integration_eng_exercise_python/s3_helper.py
+++ integration_eng_exercise_python/s3_helper.py
@@ -1,13 +1,8 @@
 """Utilities for working with AWS S3."""
-import io
-import json
 import logging
-import os
+import requests
 
 import boto3
-from urllib.parse import urlparse
-
-from botocore import exceptions as boto_exceptions
 from boto3.s3.transfer import TransferConfig
 
 
@@ -19,7 +14,7 @@ class S3Helper(object):
 
     default_config = TransferConfig()
 
-    def __init__(self, bucket_name, profile=None, config=None):
+    def __init__(self, bucket_name, region_code, profile=None, config=None):
         """Connect to S3 and perform basic config.
 
         Args:
@@ -32,6 +27,8 @@ class S3Helper(object):
         self._connect(profile=profile)
         self.bucket_name = bucket_name
         self.bucket = self.resource.Bucket(bucket_name)
+        self.region_code = region_code
+        self.default_config.region_name = region_code
         self.config = config if config else self.default_config
 
     def _connect(self, profile=None):
@@ -69,209 +66,24 @@ class S3Helper(object):
             path = path.replace('//', '/')
         return path
 
-    def download_key(self, key, dest_path):
-        """Download a single S3 key (file) to the local file system.
+    def download_key_with_presigned_url(self, key, dest_path, expiration=3600):
+        """Download a single S3 key (file) using direct HTTP access without AWS authentication.
+        Note: This will only work for public S3 buckets or objects that have been made publicly accessible.
 
         Args:
             key (str):          S3 key (everything except the bucket)
             dest_path (str):    Local destination path
+            expiration (int):   Not used, kept for backward compatibility
         """
         key = self._clean_s3_path(key)
-        self.bucket.download_file(key, dest_path, Config=self.config)
+        
+        # Construct the public S3 URL
+        url = f"https://{self.bucket_name}.s3.amazonaws.com/{key}"
 
-    def upload_key(self, key, src_path):
-        """Upload a single S3 key (file) from the local file system.
-
-        Args:
-            key (str):          S3 key (everything except the bucket)
-            src_path (str):     Local source path
-        """
-        key = self._clean_s3_path(key)
-        self.bucket.upload_file(src_path, key, Config=self.config)
-
-    def upload_json(self, key, data):
-        """Upload a data set as json.
-
-        Args:
-            key (str):     S3 key (everything except the bucket)
-            data ([]):     JSON Data set
-        """
-        key = self._clean_s3_path(key)
-        obj = self.resource.Object(self.bucket_name, key)
-        obj.put(Body=json.dumps(data))
-
-    def upload_byte_stream(self, S3_key, byte_stream):
-        """Upload a schema which is already encoded as json string by json.dumps()
-        Note the S3_key does NOT support full s3 url, e.g., "s3://..."
-        Args:
-            S3_key (str):     S3 key (everything except the bucket)
-            byte_stream (str/bytes):     Data in string or bytes representation
-        """
-        self.resource.Object(self.bucket_name, S3_key).put(Body=byte_stream)
-        logger.info("uploaded byte stream to S3 bucket {bucket} with target key {S3_key}"
-                    .format(bucket=self.bucket_name, S3_key=S3_key))
-
-    def download_prefix(self, prefix, dest_dir):
-        """Download all keys (files) within a prefix (folder) in the S3 bucket.
-
-        Preserves S3 folder structure (excluding prefix). For example:
-            Source key:     test/folder_1/folder_2/sample.txt
-            Prefix:         test/folder_1/
-            Destination:    /tmp/
-            Result:         /tmp/folder_2/sample.txt
-
-        Args:
-            prefix (str):       Prefix (folder) to download the contents of
-            dest_dir (str):     Local destination directory
-        """
-        prefix = self._clean_s3_path(prefix)
-        matching_objects = self.bucket.objects.filter(Prefix=prefix)
-        for obj in matching_objects:
-            key = obj.key
-            if not key.endswith('/'):
-                obj_rel_path = key.replace(prefix, '').lstrip('/')
-                obj_dest_path = os.path.join(dest_dir, obj_rel_path)
-                os.makedirs(os.path.dirname(obj_dest_path), exist_ok=True)
-                self.download_key(key, obj_dest_path)
-
-    def list_keys(self, prefix=None, list_objects=False):
-        """List the keys in a bucket or under a prefix.
-
-        Args:
-            prefix (str):       Prefix (folder) to list the contents of
-            list_objects (bool) : Optionally, yield objects.
-                    Default is False so keys are yielded.
-
-        Yields:
-            str:        Key name
-        """
-        if prefix:
-            prefix = self._clean_s3_path(prefix)
-            objects = self.bucket.objects.filter(Prefix=prefix)
-        else:
-            objects = self.bucket.objects.all()
-        if list_objects:
-            for obj in objects:
-                yield obj
-        else:
-            for obj in objects:
-                yield obj.key
-
-    def read_byte_stream(self, s3key):
-        try:
-            return self.resource.Object(self.bucket_name, s3key).get()['Body'].read()
-        except Exception:
-            logger.info(
-                'Failed to retrieve data from bucket {bucket} with target key {S3_key}'
-                .format(bucket=self.bucket_name, S3_key=s3key))
-            return None
-
-    def get_size(self, prefix=None):
-        """Returns total bytes of contents in a bucket or under a prefix.
-
-        :param prefix (str) : Optional. Prefix (folder) to get size of.
-        :return: total bytes of content
-        """
-        total_bytes = 0
-        for obj in self.list_keys(prefix, list_objects=True):
-            total_bytes += obj.size
-        return total_bytes
-
-    def delete_all_keys_with_prefix(self, prefix):
-        """Delete all keys under a given prefix.
-        Args:
-            prefix (str): Prefix (folder) in S3
-        """
-        keys = self.list_keys(prefix=prefix)
-        if keys:
-            logger.warning("Deleting all keys under prefix {}".format(prefix))
-            self.delete_keys(keys)
-
-    def rename_prefix(self, prefix, new_prefix):
-        """Rename S3 prefix. This method is useful for Spark dumps using
-        partitionBy which names S3 prefix as '.../{partition-field}={value}'
-        which may not be the desirable naming convention.
-
-        :param prefix (str): Original S3 prefix to rename.
-        :param new_prefix (str): New S3 prefix.
-        """
-        prefix = prefix[:-1] if prefix.endswith('/') else prefix
-        new_prefix = new_prefix[:-1] \
-            if new_prefix.endswith('/') else new_prefix
-        for key in self.list_keys(prefix):
-            filename = key.split('/')[-1]
-            new_key = '{new_prefix}/{filename}'.format(new_prefix=new_prefix,
-                                                       filename=filename)
-            source = '{bucket}/{key}'.format(bucket=self.bucket_name, key=key)
-            self.resource.Object(self.bucket_name, new_key).copy_from(
-                CopySource=source)
-            self.resource.Object(self.bucket_name, key).delete()
-
-    def list_subfolders_under_folder(self, s3folder):
-        """
-        list subfolders under given s3folder/s3prefix. Note in corner cases when the target s3folder is a leaf folder,
-        this function will return its underlying s3keys and treat them as 'subfolders'.
-
-        Parameters
-        ----------
-        s3folder : str
-                   target s3prefix/folder
-
-
-        Note
-        --------
-        to make the return consistent, if the target folder is a leaf folder and s3keys are returned,
-        then they will be appended by folder symbol '/'.
-
-        Returns
-        --------
-        iterator
-         an iterator of either keys or subfolders, note iterator can be empty.
-        """
-        result = self.client.list_objects_v2(Bucket=self.bucket_name, Prefix=s3folder, Delimiter='/')
-        folder_entries = result.get('CommonPrefixes')
-        if folder_entries:
-            for entry in folder_entries:
-                yield entry['Prefix']
-        else:
-            key_entries = result.get('Contents')
-            if key_entries:
-                for key_entry in key_entries:
-                    s3key = key_entry['Key']
-                    if s3key != s3folder:  # rule out the s3folder itself
-                        yield s3key + '/'
-
-
-    def get_all_s3_objects(self, prefix):
-        # work around AWS's limit of listing up to 1000 objects in S3 prefix
-        continuation_token = None
-        while True:
-            response = self.client.list_objects_v2(Bucket=self.bucket_name, Prefix=prefix, MaxKeys=1000,
-                                                   ContinuationToken=continuation_token) if continuation_token \
-                else self.client.list_objects_v2(Bucket=self.bucket_name, Prefix=prefix, MaxKeys=1000)
-            yield from response.get('Contents', [])
-            if not response.get('IsTruncated'):  # At the end of the list?
-                break
-            continuation_token = response.get('NextContinuationToken')
-
-
-class S3Url(object):
-    """parse s3 url into bucket and key"""
-
-    def __init__(self, url):
-        self._parsed = urlparse(url, allow_fragments=False)
-
-    @property
-    def bucket(self):
-        return self._parsed.netloc
-
-    @property
-    def key(self):
-        if self._parsed.query:
-            return self._parsed.path.lstrip('/') + '?' + self._parsed.query
-        else:
-            return self._parsed.path.lstrip('/')
-
-    @property
-    def url(self):
-        return self._parsed.geturl()
+        # Download using requests
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        # Save to file
+        with open(dest_path, 'wb') as f:
+            f.write(response.content)
